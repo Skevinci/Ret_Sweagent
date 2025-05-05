d2e8aab5fd28c909290fb984b13f5bdc110c76ac
sikaili <skevinci@umich.edu>
2025-05-04 20:29:32 -0400

    .

--- train.sh
+++ train.sh
@@ -4,10 +4,11 @@ set -x
 export N_GPUS=8
 export BASE_MODEL="/home/ec2-user/Qwen2.5-7B"
 export DATA_DIR="/home/ec2-user/Ret_Sweagent/data/auto_sweagent"
+export ROLLOUT_DIR="/home/ec2-user/Ret_Sweagent/rollout"
 export EXPERIMENT_NAME=ec2-experiment
-export REWARD_PATH="/home/ec2-user/Ret_Sweagent/verl/utils/reward_score/auto_sweagent.py"
+export REWARD_PATH="/home/ec2-user/Ret_Sweagent/verl/utils/reward_score/__init__.py"
 
-/home/ec2-user/miniconda3/envs/swe/bin/python3 -m verl.trainer.main_ppo \
+/home/ec2-user/miniconda3/envs/verl/bin/python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
@@ -30,23 +31,25 @@ export REWARD_PATH="/home/ec2-user/Ret_Sweagent/verl/utils/reward_score/auto_swe
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=False \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    reward_model.reward_manager=prime \
+    reward_model.reward_manager=naive \
     custom_reward_function.path=$REWARD_PATH \
+    custom_reward_function.name=_default_compute_score \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
+    trainer.rollout_data_dir=$ROLLOUT_DIR \
     trainer.logger=['console','wandb'] \
     trainer.project_name='ec2_train' \
-    trainer.experiment_name='train_0501' \
+    trainer.experiment_name='train_0504' \
     +trainer.val_before_train=False \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=20 \
-    trainer.test_freq=5 \
+    trainer.save_freq=5 \
+    trainer.test_freq=1 \
     trainer.default_hdfs_dir=null \
     trainer.total_epochs=15 $@
